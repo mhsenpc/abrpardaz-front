131292a161f9851b42d204912d53bbd842f45e7f
DadePardazan <mhmdx07@yahoo.com>
2020-01-15 12:15:52 +0330

    register #34

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,6 +12,10 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import axios from "axios";
+import {api_base, register} from "../Api";
+
+
 
 function Copyright() {
     return (
@@ -46,6 +50,21 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+
+function reigsterUser(event) {
+    event.preventDefault();
+    const {email, password} = event.currentTarget.elements;
+    axios.post(api_base + register, {email: email.value, password: password.value})
+        .then(res => {
+            console.log(res.data);
+            const msg = res.data.data.message;
+
+            alert(msg)
+
+        })
+}
+
+
 export default function Register() {
     const classes = useStyles();
 
@@ -57,40 +76,19 @@ export default function Register() {
                     <LockOutlinedIcon />
                 </Avatar>
                 <Typography component="h1" variant="h5">
-                    Sign up
+                    ثبت نام
                 </Typography>
-                <form className={classes.form} noValidate>
+                <form onSubmit={reigsterUser} className={classes.form} noValidate>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
-                            />
-                        </Grid>
+
+
                         <Grid item xs={12}>
                             <TextField
                                 variant="outlined"
                                 required
                                 fullWidth
                                 id="email"
-                                label="Email Address"
+                                label="پست الکترونیک"
                                 name="email"
                                 autoComplete="email"
                             />
@@ -101,18 +99,13 @@ export default function Register() {
                                 required
                                 fullWidth
                                 name="password"
-                                label="Password"
+                                label="رمز عبور"
                                 type="password"
                                 id="password"
                                 autoComplete="current-password"
                             />
                         </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                        </Grid>
+
                     </Grid>
                     <Button
                         type="submit"
@@ -121,12 +114,12 @@ export default function Register() {
                         color="primary"
                         className={classes.submit}
                     >
-                        Sign Up
+                        ثبت نام
                     </Button>
                     <Grid container justify="flex-end">
                         <Grid item>
-                            <Link href="#" variant="body2">
-                                Already have an account? Sign in
+                            <Link href="/login" variant="body2">
+                                قبلا ثبت نام کرده اید؟ ورود
                             </Link>
                         </Grid>
                     </Grid>
