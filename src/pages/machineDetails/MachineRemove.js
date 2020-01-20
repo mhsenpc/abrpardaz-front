b7636f0489a4ab531ce43c7c8235247380636147
DadePardazan <mhmdx07@yahoo.com>
2020-01-20 19:55:48 +0330

    Machine delete page #40

--- src/pages/machineDetails/MachineRemove.js
+++ src/pages/machineDetails/MachineRemove.js
@@ -6,8 +6,29 @@ import Box from '@material-ui/core/Box';
 import axios from "axios";
 import {api_base} from "../../Api";
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+
 
 function MachineRemove(props) {
+
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     function removeMachine() {
 
@@ -38,13 +59,38 @@ function MachineRemove(props) {
                             در صورت حذف ماشین ،اطلاعات آن نابود میگرددو این عملیات قابل برگشت نیست.
                         </p>
 
-                        <Button onClick={() => removeMachine()} variant="contained" color="secondary">
+                        <Dialog
+                            fullScreen={fullScreen}
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="responsive-dialog-title"
+                        >
+                            <DialogTitle id="responsive-dialog-title">{"⚠️حذف ماشین"}</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    در صورت حذف ماشین ،اطلاعات آن نابود میگرددو این عملیات قابل برگشت نیست. آیا از حذف اطمینان دارید؟
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button autoFocus onClick={handleClose} color="primary">
+                                    انصراف
+                                </Button>
+                                <Button onClick={() => removeMachine()} color="primary" autoFocus>
+                                    حذف
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+                        <Button onClick={handleClickOpen} variant="contained" color="secondary">
                            حذف ماشین
                         </Button>
 
                     </Box>
 
                 </Paper>
+
+
+
+
             </Grid>
 
         </div>
