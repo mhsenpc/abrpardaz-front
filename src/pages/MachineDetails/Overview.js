949c977df4ea5eac1b0e98c55afd4dcc03d02463
DadePardazan <mhmdx07@yahoo.com>
2020-01-28 16:41:32 +0330

    overview page design #49

--- src/pages/MachineDetails/Overview.js
+++ src/pages/MachineDetails/Overview.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from "@material-ui/core/Paper/Paper";
 import Box from '@material-ui/core/Box';
+import EuroIcon from '@material-ui/icons/Euro';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import ComputerSharpIcon from '@material-ui/icons/ComputerSharp';
+import Divider from '@material-ui/core/Divider';
 
 export default function Overview() {
     return (
+
         <div>
             <Grid item xs={12}
                   direction="row"
@@ -12,8 +18,16 @@ export default function Overview() {
             >
                 <div>
                     <h1>نمای کلی</h1>
+                    <br/>
+                    <p>
+
+                        CX31  {/*<Divider style={} orientation="vertical"  />*/}    <ComputerSharpIcon/>2vcpu   <LoyaltyIcon/> 8GB RAM  <LocalOfferIcon/> DISK LOCAL <EuroIcon/> 8.90/mo PRICE
+
+                    </p>
+                    <hr/>
                 </div>
             </Grid>
+
 
         </div>
     )
