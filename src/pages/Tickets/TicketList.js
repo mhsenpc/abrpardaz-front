2831f8772c92635a852fb4d914b8aefdb0e980c6
DadePardazan <mhmdx07@yahoo.com>
2020-02-08 12:33:42 +0330

    Layout style #52

--- src/pages/Tickets/TicketList.js
+++ src/pages/Tickets/TicketList.js
@@ -1,41 +1,47 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import {deepOrange, red} from '@material-ui/core/colors';
+import {deepOrange} from '@material-ui/core/colors';
 import axios from "axios";
 import {api_base, ticketsList} from "../../Api";
 import Button from "@material-ui/core/Button";
 import AddIcon from '@material-ui/icons/AddBox';
+import {createStyles, makeStyles, Theme} from "@material-ui/core";
 
 
-const useStyles = makeStyles({
-    card: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-        textAlign: 'right'
-    },
-    pos: {
-        marginBottom: 12,
-    },
-    orange: {
-        backgroundColor: deepOrange[500],
-    },
-    border_color: {
-        borderColor: '#f1f1f1'
-    }
-});
+
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        card: {
+            minWidth: 275,
+        },
+        paper: {
+            padding: theme.spacing(2),
+            margin: 'auto',
+        },
+        bullet: {
+            display: 'inline-block',
+            margin: '0 2px',
+            transform: 'scale(0.8)',
+        },
+        title: {
+            fontSize: 14,
+            textAlign: 'right'
+        },
+        pos: {
+            marginBottom: 12,
+        },
+        orange: {
+            backgroundColor: deepOrange[500],
+        },
+        border_color: {
+            borderColor: '#f1f1f1'
+        },
+    }));
 
 
 function TicketList() {
@@ -43,6 +49,7 @@ function TicketList() {
     const classes = useStyles();
 
     const [items, setItems] = React.useState([]);
+    const JDate = require('jalali-date');
 
     React.useEffect(() => {
         axios.get(api_base + ticketsList)
@@ -57,50 +64,51 @@ function TicketList() {
     return (
 
         <div>
-            <Grid item xs={12} container
-                  direction="row"
-                  alignItems="center"
-            >
-                <Paper>
 
-                    <Box p={2} width={700}>
 
-                        <Box width={200}>
+            <Button href={'/NewTicket'} variant="contained" color="primary">
+                <AddIcon/>
+                تیکت جدید
+            </Button>
 
-                            <Button href={'/NewTicket'} variant="contained" color="primary">
-                                <AddIcon/>
-                                تیکت جدید
-                            </Button>
 
-                            <Card className={classes.card} variant="outlined">
-                                {items.map(row => (
-                                    <CardContent key={row.id}>
-                                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                            <a href={'/TicketDetails/' + row.id.toString()}>{row.title}</a>
-                                        </Typography>
-                                        <Typography className={classes.title} variant="h5" component="h2">
-                                            {row.status}
-                                        </Typography>
-                                        <Typography className={classes.title} color="textSecondary">
-                                            {row.created_at}
-                                        </Typography>
-                                    </CardContent>
-                                ))}
+            <Paper className={classes.paper} >
+            <Grid container spacing={3}>
 
-                                {items.length == 0 &&
-                                    <p>
-                                        شما تاکنون تیکتی ایجاد نکرده اید
-                                    </p>
-                                }
-                            </Card>
-                        </Box>
-                    </Box>
 
-                </Paper>
+                    {items.map(row => (
+                        <Grid item xs={8} className='itemList'>
+
+                            <Paper className={classes.paper}   >
+                                <CardContent className={row.status} key={row.id} >
+                                    <Typography className={classes.title}  color="textSecondary" gutterBottom>
+                                        <a className='titleTicket' href={'/TicketDetails/' + row.id.toString()}>
+
+                                            {row.title}
+                                        </a>
+                                    </Typography>
+                                    <Typography className={classes.title} variant="h5" component="h2">
+                                        {row.status}
+                                    </Typography>
+                                    <Typography className={classes.title} color="textSecondary">
+                                        {(new JDate(new Date(row.created_at))).format('DD MMMM YYYY')}
+                                    </Typography>
+                                </CardContent>
+                            </Paper>
+
+                        </Grid>
+                    ))}
+
+                    {items.length == 0 &&
+                    <p>
+                        شما تاکنون تیکتی ایجاد نکرده اید
+                    </p>
+                    }
+
 
 
             </Grid>
-
+            </Paper>
 
         </div>
 
