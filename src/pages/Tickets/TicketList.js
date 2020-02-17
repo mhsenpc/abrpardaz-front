304597c73a2a9a53023bbe0d5534acc53eee2670
mohsen <mhsenpc@gmail.com>
2020-02-17 20:27:56 +0330

    Ticket list

--- src/pages/Tickets/TicketList.js
+++ src/pages/Tickets/TicketList.js
@@ -3,43 +3,25 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import {deepOrange} from '@material-ui/core/colors';
 import axios from "axios";
 import {api_base, ticketsList} from "../../Api";
 import Button from "@material-ui/core/Button";
-import AddIcon from '@material-ui/icons/AddBox';
+import AddBoxIcon from '@material-ui/icons/AddBox';
 import {createStyles, makeStyles, Theme} from "@material-ui/core";
-
-
-
+import Alert from '@material-ui/lab/Alert';
+import TableCell from "@material-ui/core/TableCell";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        card: {
-            minWidth: 275,
-        },
-        paper: {
-            padding: theme.spacing(2),
-            margin: 'auto',
-        },
-        bullet: {
-            display: 'inline-block',
-            margin: '0 2px',
-            transform: 'scale(0.8)',
+        ticketItem:{
+            marginBottom:5
         },
         title: {
             fontSize: 14,
             textAlign: 'right'
-        },
-        pos: {
-            marginBottom: 12,
-        },
-        orange: {
-            backgroundColor: deepOrange[500],
-        },
-        border_color: {
-            borderColor: '#f1f1f1'
         },
     }));
 
@@ -63,54 +45,56 @@ function TicketList() {
 
     return (
 
-        <div>
-
-
-            <Button href={'/NewTicket'} variant="contained" color="primary">
-                <AddIcon/>
-                تیکت جدید
-            </Button>
-
-
-            <Paper className={classes.paper} >
             <Grid container spacing={3}>
-
-
-                    {items.map(row => (
-                        <Grid item xs={8} className='itemList'>
-
-                            <Paper className={classes.paper}   >
-                                <CardContent className={row.status} key={row.id} >
-                                    <Typography className={classes.title}  color="textSecondary" gutterBottom>
-                                        <a className='titleTicket' href={'/TicketDetails/' + row.id.toString()}>
-
-                                            {row.title}
-                                        </a>
-                                    </Typography>
-                                    <Typography className={classes.title} variant="h5" component="h2">
-                                        {row.status}
-                                    </Typography>
-                                    <Typography className={classes.title} color="textSecondary">
-                                        {(new JDate(new Date(row.created_at))).format('DD MMMM YYYY')}
-                                    </Typography>
-                                </CardContent>
-                            </Paper>
-
-                        </Grid>
-                    ))}
-
-                    {items.length == 0 &&
-                    <p>
-                        شما تاکنون تیکتی ایجاد نکرده اید
-                    </p>
-                    }
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
+                    <Button href={'/NewTicket'} variant="contained" color="primary">
+                        <AddBoxIcon/>
+                        تیکت جدید
+                    </Button>
 
 
 
+
+                {items.map(row => (
+                    <Grid item xs={10}>
+                        <Card variant={"outlined"} className={classes.ticketItem} >
+                            <CardContent key={row.id}>
+                                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    <Grid container>
+                                        <Grid item xs={3}>
+                                            <a className='titleTicket'>
+                                                {row.title}
+                                            </a>
+                                        </Grid>
+                                        <Grid item xs={5}></Grid>
+                                        <Grid item xs={4}>
+                                            <Typography className={classes.title} color="textSecondary">
+                                                ایجاد شده در
+                                                {(new JDate(new Date(row.created_at))).format('DD MMMM YYYY')}&nbsp;
+                                                {new Date(row.created_at).toLocaleTimeString()}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Typography>
+
+                            </CardContent>
+
+                            <CardActions>
+                                <Button variant={"outlined"} color={"primary"} href={'/TicketDetails/' + row.id.toString()} size="small">بیشتر</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+
+                {items.length == 0 &&
+                <Alert severity="info">
+                    شما تاکنون تیکتی ایجاد نکرده اید
+                </Alert>
+                }
+                    </Paper>
+                </Grid>
             </Grid>
-            </Paper>
-
-        </div>
 
     );
 }
