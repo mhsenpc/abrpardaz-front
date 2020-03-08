5210cfe39e7bc056161c73d4dede9b04dc5dc430
mohsen <mhsenpc@gmail.com>
2020-03-08 12:39:00 +0330

    default project selected in create machine #68

--- src/pages/MachineDetails/Rebuild.js
+++ src/pages/MachineDetails/Rebuild.js
@@ -5,8 +5,30 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Select from "@material-ui/core/Select";
 import MenuItem from '@material-ui/core/MenuItem';
+import axios from "axios";
+import {api_base, imagesList} from "../../Api";
 
-export default function Rebuild() {
+export default function Rebuild(props) {
+    const [imageItems, setImageItems] = React.useState([]);
+    const [imageId, setImageId] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get(api_base + imagesList)
+            .then(res => {
+                const list = res.data.list;
+                setImageItems(list);
+                if(list.length>0)
+                    setImageId(list[0].id);
+            });
+    }, []);
+
+    function requestRebuild() {
+        axios.post(api_base + 'machines/' + props.id.toString() + '/rebuild'  , {image_id : imageId})
+            .then(res => {
+                props.setResponse(res.data)
+            })
+    }
+
     return (
         <div>
             <Grid item xs={12}>
@@ -19,12 +41,13 @@ export default function Rebuild() {
                         <p>
                             توجه: تمامی اطلاعات قبلی سرور حذف می گردد!
                         </p>
-                        <Select>
-                            <MenuItem>Ubuntu 16.04</MenuItem>
-                            <MenuItem>Ubuntu 18.04</MenuItem>
+                        <Select value={imageId}>
+                            {imageItems.map(row=>(
+                                <MenuItem value={row.id} >{row.name} {row.version}</MenuItem>
+                            ))}
                         </Select>
                         <br/>
-                        <Button variant="contained" color="primary">نصب مجدد</Button>
+                        <Button onClick={requestRebuild} variant="contained" color="primary">نصب مجدد</Button>
                     </Box>
                 </Paper>
 
